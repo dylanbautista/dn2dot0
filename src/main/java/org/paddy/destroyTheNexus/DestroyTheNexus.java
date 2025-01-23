05c924aeef7190ca6db88d632b55289a38b27220
dylan.bautista <dylan.bautista@estudiantat.upc.edu>
2025-01-23 20:00:03 +0100

    Add welcome message on join event.

--- src/main/java/org/paddy/destroyTheNexus/DestroyTheNexus.java
+++ src/main/java/org/paddy/destroyTheNexus/DestroyTheNexus.java
@@ -1,17 +1,33 @@
 package org.paddy.destroyTheNexus;
 
+import org.bukkit.ChatColor;
+import org.bukkit.entity.Player;
+import org.bukkit.event.Listener;
 import org.bukkit.plugin.java.JavaPlugin;
+import org.bukkit.event.player.PlayerJoinEvent;
+import org.bukkit.event.EventHandler;
 
 public final class DestroyTheNexus extends JavaPlugin {
+
+    private class JoinListener implements Listener {
+        @EventHandler
+        public void onPlayerJoin(PlayerJoinEvent event) {
+            Player player = event.getPlayer();
+            player.sendMessage(ChatColor.GOLD + "You are now destroying the Nexus, " + player.getName());
+        }
+    }
 
     @Override
     public void onEnable() {
         // Plugin startup logic
+        getLogger().info("Starting DestroyTheNexus...");
+        getServer().getPluginManager().registerEvents(new JoinListener(), this);
 
     }
 
     @Override
     public void onDisable() {
         // Plugin shutdown logic
+        getLogger().info("Stopping DestroyTheNexus...");
     }
 }
